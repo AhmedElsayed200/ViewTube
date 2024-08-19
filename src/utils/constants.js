fdcba9bde80b67636cb5236ca52f99308789f426
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-08-19 18:30:32 +0200

    icons and sidebar categories

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import MusicNote from "@mui/icons-material/MusicNote";
+import Home from "@mui/icons-material/Home";
+import Code from "@mui/icons-material/Code";
+import Video from "@mui/icons-material/OndemandVideo";
+import Gaming from "@mui/icons-material/SportsEsports";
+import LiveTV from "@mui/icons-material/LiveTv";
+import School from "@mui/icons-material/School";
+import Beauty from "@mui/icons-material/FaceRetouchingNatural";
+import Fashion from "@mui/icons-material/Checkroom";
+import Audio from "@mui/icons-material/GraphicEq";
+import Comedy from "@mui/icons-material/TheaterComedy";
+import Fitness from "@mui/icons-material/FitnessCenter";
+import Developer from "@mui/icons-material/DeveloperMode";
+
+export const logoUrl = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categoryList = [
+  { label: "New", icon: <Home /> },
+  { label: "JS Mastery", icon: <Code /> },
+  { label: "Coding", icon: <Code /> },
+  { label: "ReactJS", icon: <Code /> },
+  { label: "NextJS", icon: <Code /> },
+  { label: "Music", icon: <MusicNote /> },
+  { label: "Education", icon: <School /> },
+  { label: "Podcast", icon: <Audio /> },
+  { label: "Movie", icon: <Video /> },
+  { label: "Gaming", icon: <Gaming /> },
+  { label: "Live", icon: <LiveTV /> },
+  { label: "Sport", icon: <Fitness /> },
+  { label: "Fashion", icon: <Fashion /> },
+  { label: "Beauty", icon: <Beauty /> },
+  { label: "Comedy", icon: <Comedy /> },
+  { label: "Gym", icon: <Fitness /> },
+  { label: "Crypto", icon: <Developer /> },
+];
+
+export const thumbnailDemoUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const channelDemoUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const videoDemoUrl = "/video/GDa8kZLNhJ4";
+export const channelDemoTitle = "JavaScript Mastery";
+export const videoDemoTitle =
+  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
+export const profilePictureDemo =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
